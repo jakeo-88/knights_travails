c6b9ad5442b28867eec6b9327ccdeca7c3bf7736
Jake <jakegreen2016@gmail.com>
2021-02-18 02:09:32 -0900

    write pseudo-code

---
+++ knights_travails.rb
@@ -0,0 +1,7 @@
+# create node for the starting point
+
+# make a modified tree which has 8 children for each node
+
+# add limits to the board
+
+# write a method that does breadth traversal and counts the levels
